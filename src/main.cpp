999bf1a509a3cc77ac640a5f3b99f21b6659c1d3
CrearmeIng <jfgil@crearme.com>
2019-08-23 17:36:43 -0500

    Mejoras para ponermultiples sensores por nodo en futuras versiones

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #define CTRL_PIN PIN_D0
 #define BAUDRATE 115200
 
+#define smplTm    200   //ms
+
 //#define DEBUG
 
 TFT_eSPI tft = TFT_eSPI();
@@ -15,6 +17,8 @@ ModbusMaster node1;
 ModbusMaster node2;
 
 bool state = true;
+long lstTm = 0;
+long currTm = 0;
 
 union unFlt {
   float flt;
@@ -31,13 +35,14 @@ void postTransmission()
   digitalWrite(CTRL_PIN, 0);
 }
 
-float getSensVal(ModbusMaster node, int32_t poX, int32_t poY){
-  uint16_t result = node.readInputRegisters(0,2);
-  if (result == node.ku8MBSuccess) {
-    ufl.int16[0] = node.getResponseBuffer(0);
-    ufl.int16[1] = node.getResponseBuffer(1);
-    float valkPa = ufl.flt;                 // Value in kPa
-    float valinh2o = valkPa * 4.01865;                   // value converted from kPa to inh2o
+void showSensVal(float valkPa, int32_t poX, int32_t poY){
+  if (valkPa == NULL) {
+    tft.setTextSize(2);
+    tft.fillRect(poX+25, poY, 116, 55, TFT_BLACK);
+    tft.drawString("No Disp", poX+25, poY + 25);
+    delay(50);
+  } else {
+    float valinh2o = valkPa * 4.01865;
     //tft.setTextColor(TFT_WHITE,TFT_BLACK);
     tft.setTextSize(1);
     tft.drawString("kPa",poX,poY+10);
@@ -50,12 +55,23 @@ float getSensVal(ModbusMaster node, int32_t poX, int32_t poY){
     tft.drawFloat(valinh2o, 3, poX+25, poY+40);
     //tft.drawNumber(sensor, poX, poY);
     delay(50);
+  }
+}
+
+float getSensValKpa(ModbusMaster node, uint8_t sensNum){
+  uint16_t reg = 0;
+  if (sensNum == 0){
+    reg = 0x00DA;
+  } else if (sensNum == 1){
+    reg = 0x00DC;
+  }
+  uint16_t result = node.readInputRegisters(reg,2);
+  if (result == node.ku8MBSuccess) {
+    ufl.int16[0] = node.getResponseBuffer(0);
+    ufl.int16[1] = node.getResponseBuffer(1);
+    float valkPa = ufl.flt;                           // Value in kPa
     return valkPa;
   } else {
-    tft.setTextSize(2);
-    tft.fillRect(poX+25, poY, 116, 55, TFT_BLACK);
-    tft.drawString("No Disp", poX+25, poY + 25);
-    delay(50);
     return NULL;
   }
 }
@@ -82,17 +98,11 @@ void setup() {
 
 
 void loop() {
-  //uint8_t result;
-  /*result = node1.writeSingleCoil(3, false);
-  delay(50);
-  Serial.print("Resultado: "); Serial.println(result,HEX);
-  delay(500);
-  result = node1.writeSingleCoil(3, true);
-  delay(50);
-  Serial.print("Resultado: "); Serial.println(result,HEX);
-  delay(500);*/
-  //getSensVal(node1, 10, 120);
-  getSensVal(node1, 80, 100);
-  //getSensVal(node2, 200, 120);
-  delay(200);
+  currTm = millis();
+  if (abs(currTm - lstTm) >= smplTm){
+    // showSensVal(node1, 10, 120);
+    showSensVal(getSensValKpa(node1,0), 80, 100);
+    //showSensVal(node2, 200, 120);
+    lstTm = currTm;
+  }
 }
